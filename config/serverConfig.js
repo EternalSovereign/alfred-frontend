04aaabf36b8b0d91430eb53eaadb636ad7e30a25
EternalSovereign <dipendrakumarbm5@gmail.com>
2025-02-19 12:32:23 +0530

    1st-commit

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -1,3 +1,3 @@
-export const server = "https://alfred-backend-beta.vercel.app/";
+export const server = "https://alfred-backend-beta.vercel.app";
 export const API_URL = `${server}/flashcards`;
 export const AUTH_URL = `${server}/auth`;
